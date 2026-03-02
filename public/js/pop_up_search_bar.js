6cda4c4a1476b732a5d76d8bd8920bf1f7ceb96e
Milunovic Dragan <milunovicdragan36@gmail.com>
2026-03-02 03:04:18 +0100

    Feature: centered popup search modal with overlay

---
+++ public/js/pop_up_search_bar.js
@@ -0,0 +1,62 @@
+(function ($) {
+
+function getPopup(){
+  return $(".pop_up_menu.milun-popup-center").first();
+}
+
+function openPopup(){
+  const $popup = getPopup();
+  if(!$popup.length) return;
+
+  $popup.addClass("is-open");
+  $("body").addClass("milun-popup-lock");
+
+  setTimeout(function(){
+    $popup.find(".search-term-menu").first().focus();
+  },80);
+}
+
+function closePopup(){
+  const $popup = getPopup();
+  if(!$popup.length) return;
+
+  $popup.removeClass("is-open");
+  $("body").removeClass("milun-popup-lock");
+}
+
+/* OPEN */
+$(document).on(
+  "click",
+  "#open-search-flyout-before-title",
+  function(e){
+    e.preventDefault();
+    openPopup();
+});
+
+/* CLOSE ICON */
+$(document).on(
+  "click",
+  "#close-search-flyout-before-title",
+  function(e){
+    e.preventDefault();
+    closePopup();
+});
+
+/* CLICK OUTSIDE CLOSE */
+$(document).on(
+  "click",
+  ".pop_up_menu.milun-popup-center",
+  function(e){
+    if(!$(e.target).closest(".notification-container").length){
+      closePopup();
+    }
+});
+
+/* ESC CLOSE */
+$(document).on("keydown",function(e){
+  if(e.key === "Escape"){
+    closePopup();
+  }
+});
+
+})(jQuery);
